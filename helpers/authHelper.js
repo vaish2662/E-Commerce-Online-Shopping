99c620d843d078662690951d2852bc92ffd797c9
vaish2662 <85391336+vaish2662@users.noreply.github.com>
2025-06-06 11:46:20 +0530

    Update authHelper.js

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -3,7 +3,6 @@ const hashPassword=async(password)=>{
     try{
         const saltRounds=10
         const hashedPassword=await bcrypt.hash(password,saltRounds)
-        console.log("hashedPassword -> ",hashedPassword)
         return hashedPassword
     }catch(error){
         console.log(error);
@@ -11,7 +10,6 @@ const hashPassword=async(password)=>{
 }
 
 const comparePassword=async (password,hashedPassword)=>{
-    console.log(await bcrypt.compare(password,hashedPassword))
     return await bcrypt.compare(password,hashedPassword)
 }
 module.exports={hashPassword,comparePassword}
